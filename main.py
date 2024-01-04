a0de96c3e494dfefba218ebdcfca23cf9b353052
Andre Kuney <akuney1@gmail.com>
2024-01-04 15:09:05 -0500

    Spacing issue

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	print("Printing out for observability purposes.")
+    print("Printing out for observability purposes.")
     return jsonify({"Message": "Is this branch going to auto-build?"})
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
